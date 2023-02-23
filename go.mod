8e5c01eaf7f1f1dc4f7af1e8c90913624d4d79f0
gustonecrush <codeboxezz@gmail.com>
2023-02-23 17:40:32 +0700

    use module created

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gustonecrush/app-go-modules
+
+go 1.18
+
+require github.com/gustonecrush/go-modules v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	golang_modules "github.com/gustonecrush/go-modules"
+)
+
+func main() {
+	fmt.Println(golang_modules.SayHello())
+}
